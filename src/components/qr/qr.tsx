20fae76e025ea40c794d96da89070a19c4924b10
MatheusASoares <matheusalso@live.com>
2023-07-25 15:53:52 -0300

    Creating QRCode

---
+++ src/components/qr/qr.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+export function QrCode() {
+  return (
+    <main className="container flex justify-center items-center h-screen">
+      <div className="flex flex-col items-center justify-center space-y-5 bg-white rounded-xl p-3 max-w-xs mx-auto">
+        <div className="rounded-xl overflow-hidden">
+          <Image
+            src="/images/qrcode.png"
+            alt="Qr Code"
+            width={350}
+            height={350}
+          />
+        </div>
+        <div className="flex flex-col justify-center items-center w-auto ">
+          <h1 className="text-center text-xl font-bold text-darkBlue mb-2">
+            Improve your front-end skills by building projects
+          </h1>
+          <p className="text-center text-grayishBlue">
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </p>
+        </div>
+      </div>
+    </main>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      white: "hsl(0, 0%, 100%)",
+      lightGray: "hsl(212, 45%, 89%)",
+      grayishBlue: "hsl(220, 15%, 55%)",
+      darkBlue: "hsl(218, 44%, 22%)",
+    },
+    screens: {
+      xs: "375px",
+      // => @media (min-width: 375px) { ... }
+
+      sm: "575px",
+      // => @media (min-width: 576px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
+
+      "2xl": "1440px",
+      // => @media (min-width: 1440px) { ... }
     },
   },
   plugins: [],
-}
+};
