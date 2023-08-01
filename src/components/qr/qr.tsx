8d413abc21b2c89dd68e67cf113befb2df4b647c
MatheusASoares <matheusalso@live.com>
2023-08-01 10:52:26 -0300

    Fixing some problems

--- src/components/qr/qr.tsx
+++ src/components/qr/qr.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export function QrCode() {
   return (
-    <main className="container flex justify-center items-center h-screen bg-lightGray">
-      <div className="flex flex-col items-center justify-center space-y-5 bg-white rounded-xl p-3 max-w-xs mx-auto">
-        <div className="rounded-xl overflow-hidden">
+    <main className="container flex justify-center items-center h-screen gap-1 bg-lightGray">
+      <div className="flex flex-col items-center justify-center space-y-5 bg-white rounded-3xl p-3 max-w-xs gap-1">
+        <div className="rounded-3xl overflow-hidden">
           <Image
             src="/images/qr/qrcode.png"
             alt="Qr Code"
@@ -12,8 +12,8 @@ export function QrCode() {
             height={350}
           />
         </div>
-        <div className="flex flex-col justify-center items-center w-auto ">
-          <h1 className="text-center text-xl font-bold text-darkBlue mb-2">
+        <div className="flex flex-col justify-center items-center w-auto gap-1">
+          <h1 className="text-center text-xl font-bold text-darkBlue">
             Improve your front-end skills by building projects
           </h1>
           <p className="text-center text-grayishBlue">
